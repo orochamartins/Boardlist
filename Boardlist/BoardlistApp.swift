411171bc878fbfa4eedc78565aacc4ce2fe9aa48
Renato Martins <rochamartinsrenato@gmail.com>
2023-08-29 22:50:52 +0100

    Initial Commit

---
+++ Boardlist/BoardlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoardlistApp.swift
+//  Boardlist
+//
+//  Created by Renato Martins on 29/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct BoardlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
