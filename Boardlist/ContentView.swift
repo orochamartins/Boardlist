5fc0ab7f3a56a5cc064eeeb85c5db7e05d5de836
Renato Martins <rochamartinsrenato@gmail.com>
2023-08-31 23:36:37 +0100

    - Added basic list structure using light wireframes (first phase)

--- Boardlist/ContentView.swift
+++ Boardlist/ContentView.swift
@@ -10,15 +10,54 @@ import SwiftUI
 struct ContentView: View {
     
     let boards: [Boards] = Bundle.main.decode("boards.json")
+    let columns = [GridItem(.flexible()), GridItem(.flexible())]
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text(boards[0].brand)
+        NavigationView {
+            ZStack {
+                LinearGradient(colors: [Color(#colorLiteral(red: 0, green: 0.02060918883, blue: 0.02991674364, alpha: 1)), Color(#colorLiteral(red: 0, green: 0.08004814165, blue: 0.1161996114, alpha: 1))], startPoint: .top, endPoint: .bottom).ignoresSafeArea()
+                
+                ScrollView {
+                    LazyVGrid(columns: columns) {
+                        ForEach(boards) { board in
+                            NavigationLink {
+                                
+                            } label: {
+                                VStack {
+                                    Circle()
+                                        .fill(.blue)
+                                        .frame(width: 100, height: 100)
+                                        .padding()
+                                    VStack(alignment: .leading, spacing: 6) {
+                                        Text(board.brand)
+                                            .font(.caption)
+                                            .foregroundColor(.primary.opacity(0.5))
+                                        Text(board.model)
+                                            .font(.headline)
+                                            .foregroundColor(.white)
+                                    }
+                                    .frame(maxWidth: .infinity)
+                                    .padding(.bottom)
+                                }
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                                .overlay(RoundedRectangle(cornerRadius: 10).stroke(.white.opacity(0.3)))
+                            }
+                        }
+                    }
+                    .padding([.horizontal, .bottom])
+                }
+                .toolbar {
+                    ToolbarItem(placement: .principal) {
+                        HStack {
+                            Image(systemName: "triangle")
+                            Text("Boardlist")
+                        }
+                    }
+                }
+                .preferredColorScheme(.dark)
+                .navigationBarTitleDisplayMode(.inline)
+            }
         }
-        .padding()
     }
 }
 
